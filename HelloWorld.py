2a33223bd65e90327286094548eb34b3daa61ef5
yshi <yshi@aquiire.com>
2019-03-02 19:29:54 -0500

    standard library

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,15 @@
-print ("HelloWorld")
+#Module + functions = the standard library
+import os
+import datetime
+import time
+
+print (os.getcwd())
+print (os.environ)
+print (os.getenv('HOME'))
+
+
+print (datetime.date.today())
+print (datetime.date.isoformat(datetime.date.today()))
+
+print (time.strftime("%H:%M"))
+print (time.strftime("%A %P"))
